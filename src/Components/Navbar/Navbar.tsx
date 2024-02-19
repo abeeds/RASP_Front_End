7619bbbbc1097cdfa653a571f822b8ab0b65517c
abeeds <shahriaabeed@gmail.com>
2024-02-18 22:35:16 -0500

    Redesigned the navbar

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 interface Page {
   destination: string;
@@ -6,7 +6,6 @@ interface Page {
 }
 
 const PAGES: Page[] = [
-  { label: 'Home', destination: '/login'},
   { label: 'Users', destination: '/users'},
   { label: 'Chatrooms', destination: '/chatrooms'},
 ]
@@ -14,15 +13,37 @@ const PAGES: Page[] = [
 function Navbar() {
 
   const mapper = (page: Page) => (
-    <li><Link to={page.destination}><button>{page.label}</button></Link></li>
+    <a className="nav-item nav-link" href={page.destination}>{page.label}</a>
   );
 
   return (
-    <nav>
-      <ul className="wrapper">
-        {PAGES.map(mapper)}
-      </ul>
-    </nav>
+    <header className="site-header">
+      <nav className="navbar navbar-expand-md navbar-dark navigBG fixed-top">
+        {/* Appears on mobile and thinner windows */}
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="container">
+          {/* Home Page */}
+          <a className="navbar-brand mr-4" href="/login"><strong>RASP</strong></a>
+          
+          {/* Nav Toggler */}
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggle" aria-controls="navbarToggle" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          {/* Main Nav Pages */}
+          <div className="collapse navbar-collapse" id="navbarToggle">
+            <div className="navbar-nav mr-auto">
+              {PAGES.map(mapper)}
+            </div>
+            
+            {/* Right side of navbar*/}
+            <div className="navbar-nav">
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
   )
 }
 
