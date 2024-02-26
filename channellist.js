d9c331c7112ca9d0f61796d4d72015e7786e2de9
pablohop <71025955+pablohop@users.noreply.github.com>
2024-02-25 20:32:17 -0500

    Made a channel list for the home page
    Made a list of channels someone can go to if they were to use the app. will edit later.

---
+++ channellist.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ChannelList = () => {
+  return (
+    <div>
+      <h3>Channels</h3>
+      <ul>
+        <li>General</li>
+        <li>Random</li>
+        {/* Add more channels here */}
+      </ul>
+    </div>
+  );
+};
+
+export default ChannelList;
