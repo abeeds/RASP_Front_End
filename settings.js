d8b1d3cfe0320f58d685e2e61c1cfa9d01235867
pablohop <71025955+pablohop@users.noreply.github.com>
2024-03-03 19:27:26 -0500

    added settings page

---
+++ settings.js
@@ -0,0 +1,18 @@
+// src/components/Settings.js
+import React from 'react';
+import styled from 'styled-components';
+
+const Settings = () => {
+  return (
+    <SettingsContainer>
+      <h2>Settings</h2>
+      {/* Add settings options here */}
+    </SettingsContainer>
+  );
+};
+
+const SettingsContainer = styled.div`
+  /* Add styles here */
+`;
+
+export default Settings;
