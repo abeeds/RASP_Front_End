f0b8cd5e4c4ff30ca3dd6888a47a273ba743679a
Shahria Abeed <105658920+abeeds@users.noreply.github.com>
2024-05-04 13:55:26 -0400

    grouped user endpoints together

--- src/constants.ts
+++ src/constants.ts
@@ -6,11 +6,11 @@ export const ADMIN_KEY = 'avespaisnotamotorcycle'
 export const GET_FORMS = BACKEND_URL + "/get_forms";
 export const UPDATE_PASS = BACKEND_URL + "/update_password";
 
-export const BAN = BACKEND_URL + "/ban/"; // username
-export const DEACTIVATE = BACKEND_URL + "/deactivate/";
-export const GET_USERS = BACKEND_URL + "/get_users";
-export const LOGIN = BACKEND_URL + "/login/"; // name/password
-export const REGISTER = BACKEND_URL + "/register/" // name/password
+export const BAN = BACKEND_URL + "/users/ban/"; // username
+export const DEACTIVATE = BACKEND_URL + "/users/deactivate/";
+export const GET_USERS = BACKEND_URL + "/users/get_users";
+export const LOGIN = BACKEND_URL + "/users/login/"; // name/password
+export const REGISTER = BACKEND_URL + "/users/register/" // name/password
 
 export const CHATROOMS_URL = BACKEND_URL + "/chatrooms";
 export const MSG_URL = BACKEND_URL + "/messages";
